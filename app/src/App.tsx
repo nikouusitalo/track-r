8efc63c1a66e9a13622291e11df63cf0c9f088ab
Niko <niko.u@hotmail.fi>
2023-01-31 15:31:03 +0200

    tailwind setup

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,21 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+     <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+        </div>
   );
 }
 
